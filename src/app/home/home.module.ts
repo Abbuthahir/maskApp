bfd6e71e1089fb057f417b77dd88527df076edef
Abbuthahir <abbuthahir04@gmail.com>
2023-08-10 11:16:16 +0530

    initial

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { MaskitoModule } from '@maskito/angular';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MaskitoModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MaskitoElementPredicateAsync, MaskitoOptions, maskitoTransform } from '@maskito/core';
+import maskitoOptions from './mask';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  a = '';
+  b = '';
 
-  constructor() {}
+  readonly options = maskitoOptions;
+
+  @ViewChild('cvv') input!: HTMLIonInputElement;
+  ngAfterViewInit() {
+    this.input.getInputElement().then((el) => {
+      el.addEventListener('maskitoReject', () => {
+        alert('Invalid Character');
+      })
+    })
+  }
+
+  readonly digitsOnlyMask: MaskitoOptions = {
+    mask: /^\d+$/,
+  };
+  readonly timeMask: MaskitoOptions = {
+    mask: [/\d/, /\d/, ':', /\d/, /\d/],
+  };
+
+  readonly predicate: MaskitoElementPredicateAsync = async (el) => (el as HTMLIonInputElement).getInputElement();
+
+  readonly numberMask: MaskitoOptions = {
+    mask: /^\d+(,\d*)?$/,
+    preprocessors: [
+      ({ elementState, data }, actionType) => {
+        const { value, selection } = elementState;
+
+        return {
+          elementState: {
+            selection,
+            value: value.replace('.', ','),
+          },
+          data: data.replace('.', ','),
+        };
+      },
+    ]
+  };
+
+  readonly uppercaseMask: MaskitoOptions = {
+    mask: /^[a-zA-Z\s]+$/,
+    postprocessors: [
+      ({ value, selection }) => ({ value: value.toUpperCase(), selection }),
+    ],
+  };
+
+  constructor() { }
+
+  setValue() {
+    this.a = maskitoTransform('abdcdkaldf12345', this.digitsOnlyMask);
+  }
 
 }
